12014c61fedfd63b9dfba15b50a6c8f3b7b379b0
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-04-21 14:51:23 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <form className="form js-form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">
+                    Kalkulator Walut
+                </legend>
+            <p>
+                <label>
+                    <span className="form__labelText">Kwota w zł*:</span>
+                    <input className="form__field js-amount" type="number" name="value" required placeholder="Mam" step="0.01"/>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">Waluta:</span>
+                        <select className="form__field js-currency" name="Currency">
+                            <option title="Euro" value="EUR">EUR</option>
+                            <option title="Dolar Amerykański" value="USD">USD</option>
+                            <option title="Funt Brytyjski" value="GBP">GBP</option>
+                            <option title="Frank Szwajcarski" value="CHF">CHF</option>
+                    </select>
+                </label>
+            </p>
+            </fieldset>
+            <p>
+                <button className="form__button">Przelicz</button>
+            </p>
+            <p className="paragraph">
+                <strong className="js-result">N/A</strong>
+            </p>
+        </form>
     </div>
   );
 }
