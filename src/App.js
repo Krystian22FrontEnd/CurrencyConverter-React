50930fada8d1a2731cf11420bbb62ae739e8f472
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-04-21 19:09:11 +0200

    added Form and Container components with style.css

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
+import Form from "./Form";
+import Container from "./Container";
+
 function App() {
   return (
-    <div className="container">
-      <form className="form">
-        <fieldset className="form__fieldset">
-          <legend className="form__legend">Kalkulator Walut</legend>
-          <p>
-            <label>
-              <span className="form__labelText">Kwota w zł*:</span>
-              <input
-                className="form__field"
-                type="number"
-                name="value"
-                required
-                placeholder="Mam"
-                step="0.01"
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span className="form__labelText">Waluta:</span>
-              <select className="form__field" name="Currency">
-                <option title="Euro" value="EUR">
-                  EUR
-                </option>
-                <option title="Dolar Amerykański" value="USD">
-                  USD
-                </option>
-                <option title="Funt Brytyjski" value="GBP">
-                  GBP
-                </option>
-                <option title="Frank Szwajcarski" value="CHF">
-                  CHF
-                </option>
-              </select>
-            </label>
-          </p>
-        </fieldset>
-        <p>
-          <button className="form__button">Przelicz</button>
-        </p>
-        <p className="paragraph">
-          <strong className="result">N/A</strong>
-        </p>
-      </form>
-    </div>
+    <Container>
+      <Form />
+    </Container>
   );
 }
 
