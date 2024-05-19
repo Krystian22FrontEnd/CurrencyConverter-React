2b4db8d395e779627480541791f01bf16a8ef5d0
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-05-19 14:55:52 +0200

    added array of currencies

---
+++ src/currencies.js
@@ -0,0 +1,12 @@
+const currencies = [
+  { id: 1, content: "Euro", shrt: "EUR", worth: 4.59 },
+  { id: 2, content: "Dolar Amerykański", shrt: "USD", worth: 4.19 },
+  { id: 3, content: "Funt Brytyjski", shrt: "GBP", worth: 5.22 },
+  { id: 4, content: "Frank Szwajcarski", shrt: "CHF", worth: 4.66 },
+];
+
+
+export default currencies;
+
+
+
