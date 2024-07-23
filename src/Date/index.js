af5ff0b4eb2008959e18440a1d344ef20a2291af
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-07-23 19:20:45 +0200

    now clock shows up immediately after page is opened

--- src/Date/index.js
+++ src/Date/index.js
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react";
 import "./style.css";
+import { interval } from "date-fns";
 
 const Time = () => {
-  const [myTime, setMyTime] = useState("");
+  const [myTime, setMyTime] = useState(new Date());
   useEffect(() => {
-    setInterval(() => {
-      const myDate = new Date();
-      const localDate = myDate.toLocaleDateString("pl-PL", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      });
-      setMyTime(localDate);
-    }, 0);
+    const intervalId = setInterval(() => {
+      setMyTime(new Date(), 1000);
+
+      return () => {clearInterval(intervalId)}
+    });
+  }, [myTime]);
+
+  const localTime = myTime.toLocaleTimeString("pl-PL", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
   });
 
-  return <div className="clock">Dzisiaj jest {myTime}</div>;
+  return <div className="clock">Dzisiaj jest {localTime}</div>;
 };
 
 export default Time;
