e03f04c99e03a12fe18531e753b3f2106911e7fa
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-08-13 12:43:09 +0200

    added theme from styled components

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        white: "hsl(0, 0%, 100%)",
+        silver: "hsl(204, 3%, 70%)",
+        lightGrey: "hsl(30, 3%, 69%)",
+        graphit: "hsl(180, 2%, 37%)",
+        orange: "hsl(29, 84%, 53%)",
+        lightOrange: "hsl(29, 84%, 60%)",
+        black: "hsl(0, 0%, 0%)",
+    }
+};
