5a18a0948b1df2b448bbb644739e1e0c3c5417af
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-08-13 12:42:14 +0200

    added global styles instead of index.css

---
+++ src/GlobalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background.jpg"
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, *::before, *::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: "Lato", sans-serif;
+  background-image: url("${background}");
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh;
+}
+`;
