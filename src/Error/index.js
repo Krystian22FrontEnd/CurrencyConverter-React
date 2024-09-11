f38098fb6862eebdd013eb1cd2ff922ea96d11ff
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-09-11 14:40:50 +0200

    added Error component

---
+++ src/Error/index.js
@@ -0,0 +1,7 @@
+import { ErrorParagraph } from "../Loading/styled";
+
+export const Error = () => {
+return (
+    <ErrorParagraph>Coś poszło nie tak.<br/> Spokojnie to pewnie wina serwera.</ErrorParagraph>
+)
+};
