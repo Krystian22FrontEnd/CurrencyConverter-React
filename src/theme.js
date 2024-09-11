8406adbf4d3ea55a15c4f59fd98fa24c02d96ad3
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-09-11 14:35:30 +0200

    added some new colours and renamed older ones

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,15 @@
 export const theme = {
-    colors: {
-        white: "hsl(0, 0%, 100%)",
-        silver: "hsl(204, 3%, 70%)",
-        lightGrey: "hsl(30, 3%, 69%)",
-        graphit: "hsl(180, 2%, 37%)",
-        orange: "hsl(29, 84%, 53%)",
-        lightOrange: "hsl(29, 84%, 60%)",
-        black: "hsl(0, 0%, 0%)",
-    }
+  colors: {
+    white: "hsl(0, 0%, 100%)",
+    silver: "hsl(0, 0%, 80%)",
+    alto: "hsl(0, 0%, 90%)",
+    graphit: "hsl(0, 0%, 50%)",
+    westSide: "hsl(29, 100%, 55%)",
+    neonCarrot: "hsl(29, 100%, 60%)",
+    jaffa: "hsl(29, 84%, 60%)",
+    black: "hsl(0, 0%, 0%)",
+    tundora: "hsl(0, 0%, 25%)",
+    red: "hsl(0, 100%, 50%)",
+    monaLisa: "hsl(0, 100%, 90%)",
+  },
 };
