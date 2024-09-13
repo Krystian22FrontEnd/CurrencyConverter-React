1c653fa037df34fe4f5520d4651e740efb2a9e48
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-09-13 11:02:51 +0200

    renamed custom hook to useRatesData

--- src/useRatesData.js
+++ src/useRatesData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export const useAsyncFunction = () => {
+export const useRatesData = () => {
   const [ratesData, setRatesData] = useState({
     status: "loading",
   });
@@ -9,7 +9,7 @@ export const useAsyncFunction = () => {
   useEffect(() => {
     const axiosData = async () => {
       try {
-        const response = await axios.get("currencies.json");
+        const response = await axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_nWs6fc91YllPpvFWEnlCJ4b93mEhsWHDew0NC9Pv&base_currency=PLN');
         const date = new Date(
           response.data.meta.last_updated_at
         ).toLocaleDateString();
